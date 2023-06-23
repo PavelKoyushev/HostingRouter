30a3847712cfe0b379c658ced0e20b3eca4ccbf1
Pavel Koyushev <pkoyushev@gmail.com>
2023-06-23 17:15:55 +0300

    Initial Commit

---
+++ HostingRouter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HostingRouter
+//
+//  Created by Pavel Koyushev on 23.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
